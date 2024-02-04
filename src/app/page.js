33145ea4f019fe3ec02580dbfd673ff026a511c8
Laurent De La Calle <laurentdelacalle@hotmail.com>
2024-02-04 21:39:43 +0100

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,186 @@
+"use client";
+
+import { data } from "@/components/data";
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
+import { useState } from "react";
+import arrow_left from "../assets/arrow_back.svg";
+import arrow_right from "../assets/arrow_forward.svg";
 
 export default function Home() {
+  const [current, setCurrent] = useState(0);
+
+  const bgVariant = {
+    initial: {
+      opacity: 1,
+      y: "100%",
+    },
+    enter: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+    exit: {
+      opacity: 0,
+      y: "-100%",
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
+  const textVariant = {
+    initial: {
+      opacity: 0,
+      x: 100,
+    },
+    enter: (current) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        // duration: 0.5,
+        delay: current,
+      },
+    }),
+    exit: (current) => ({
+      opacity: 0,
+      x: -100,
+      transition: {
+        // duration: 0.5,
+        delay: current,
+      },
+    }),
+  };
+
+  const miniVariant = {
+    initial: {
+      opacity: 0,
+      y: "-100%",
+    },
+    enter: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.4,
+      },
+    },
+    exit: {
+      opacity: 0,
+      y: "100%",
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
+  const handleClickRight = () => {
+    if (current === data.length - 1) {
+      setCurrent(0);
+    } else {
+      setCurrent(current + 1);
+    }
+  };
+
+  const handleClickLeft = () => {
+    if (current === 0) {
+      setCurrent(2);
+    } else {
+      setCurrent(current - 1);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="h-screen w-full container mx-auto flex justify-center items-center">
+      <AnimatePresence mode="popLayout">
+        <div
+          key={current}
+          className="w-full h-[600px] rounded-3xl relative overflow-hidden"
+          style={{ backgroundColor: data[current].bgColor }}
+        >
+          <motion.div
+            className="h-[600px] w-[75%] relative p-10"
+            variants={bgVariant}
+            initial="initial"
+            animate="enter"
+            exit="exit"
           >
-            By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={data[current].bg}
+              alt=""
+              fill
+              className="opacity-50 rounded-l-3xl"
             />
-          </a>
+          </motion.div>
+
+          <div className="flex items-center justify-between gap-20 absolute top-0 w-full h-full pl-10">
+            <motion.div className="flex flex-col gap-6 text-white">
+              <motion.p
+                className="pl-3"
+                variants={textVariant}
+                custom={0.2}
+                initial="initial"
+                animate="enter"
+                exit="exit"
+              >
+                {data[current].tag}
+              </motion.p>
+              <motion.h1
+                className="text-[66px] font-bold"
+                variants={textVariant}
+                custom={0.3}
+                initial="initial"
+                animate="enter"
+                exit="exit"
+              >
+                {data[current].title}
+              </motion.h1>
+              <motion.p
+                className="text-[20px]"
+                variants={textVariant}
+                custom={0.4}
+                initial="initial"
+                animate="enter"
+                exit="exit"
+              >
+                {data[current].desc}
+              </motion.p>
+            </motion.div>
+
+            <div>
+              <motion.div
+                variants={miniVariant}
+                className="mr-60 h-[400px] w-[300px] relative"
+                initial="initial"
+                animate="enter"
+                exit="exit"
+              >
+                <Image
+                  src={data[current].mini}
+                  alt=""
+                  fill
+                  className="rounded-3xl shadow-xl"
+                />
+              </motion.div>
+            </div>
+          </div>
+          <div className="flex gap-10 absolute bottom-10 left-[71%]">
+            <div
+              className="bg-slate-50 cursor-pointer flex items-center w-[40px] h-[40px] justify-center rounded-full relative"
+              onClick={handleClickLeft}
+            >
+              <Image src={arrow_left} alt="" height={30} width={30} />
+            </div>
+            <div
+              className="bg-slate-50 cursor-pointer flex items-center w-[40px] h-[40px] justify-center rounded-full relative"
+              onClick={handleClickRight}
+            >
+              <Image src={arrow_right} alt="" height={30} width={30} />
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </AnimatePresence>
     </main>
   );
 }
---
+++ src/components/data.js
@@ -0,0 +1,36 @@
+import mini1 from "/public/img/mini1.jpg";
+import mini2 from "/public/img/mini2.jpg";
+import mini3 from "/public/img/mini3.jpg";
+import slide1 from "/public/img/slide1.jpg";
+import slide2 from "/public/img/slide2.jpg";
+import slide3 from "/public/img/slide3.jpg";
+
+export const data = [
+  {
+    id: 1,
+    title: "Explore New Horizons",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi laborum tempore praesentium. Veritatis illo quia cupiditate earum voluptate iste natus ad neque, doloribus fugit culpa, provident quibusdam laborum ratione harum.",
+    bg: slide1,
+    mini: mini1,
+    tag: "Beach",
+    bgColor: "#61A0C7",
+  },
+  {
+    id: 2,
+    title: "Discover New Places",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi laborum tempore praesentium. Veritatis illo quia cupiditate earum voluptate iste natus ad neque, doloribus fugit culpa, provident quibusdam laborum ratione harum.",
+    bg: slide2,
+    mini: mini2,
+    tag: "Desert",
+    bgColor: "#C7A061",
+  },
+  {
+    id: 3,
+    title: "Evade the Ordinary",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi laborum tempore praesentium. Veritatis illo quia cupiditate earum voluptate iste natus ad neque, doloribus fugit culpa, provident quibusdam laborum ratione harum.",
+    bg: slide3,
+    mini: mini3,
+    tag: "city",
+    bgColor: "#808080",
+  },
+];
